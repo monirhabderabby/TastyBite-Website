68caa0eb09aaa318f2e43856aaec44ced55d041c
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-20 18:45:18 +0600

    feat: develop Logo component in Logo.tsx Created a reusable Logo component using TypeScript and React Applied styling for different screan sized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Logo from "@/components/common/logo/Logo";
+
 export default function Home() {
-  return <section className="bg-primary-brack container"></section>;
+  return (
+    <section className="min-h-screen bg-primary-black w-full flex justify-start items-start px-10">
+      <div className="h-[60px]  flex items-center">
+        <Logo />
+      </div>
+    </section>
+  );
 }
