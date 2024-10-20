a6dc5b5e6fca52920bd931db4b58ed4c6e6bb9c4
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-20 14:29:05 +0600

    remove: h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <section className="bg-primary-brack container">
-      <h1 className="uppercase text-[70px] text-white font-bold">
-        OUR SPECIALITY
-      </h1>
-    </section>
-  );
+  return <section className="bg-primary-brack container"></section>;
 }
