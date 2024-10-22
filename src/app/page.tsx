522448a459b9b6954896c3208a716a51c69ab563
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-22 16:45:01 +0600

    feat: develop BannerInfo component with responsive design and dynamic data
    - Created BannerInfo component to display information with icons, titles, and descriptions
    - Implemented responsive grid layout for optimal display on mobile and desktop
    - Integrated Image component for optimized image handling
    - Built reusable BannerInfoCard component to dynamically render data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-import Hero from "@/components/ui/Hero";
+import Banner from "@/components/ui/banner";
+import BannerInfo from "@/components/ui/BannerInfo";
 
 export default function Home() {
   return (
-    <section
-      style={{ backgroundImage: `url(/images/banner-bg.webp)` }}
-      className=" min-h-[50vh] md:min-h-[70vh] lg:min-h-[90vh] bg-primary-black w-full relative overflow-hidden"
-    >
-      <div className="pt-[100px]">
-        <h1 className="uppercase text-[50px] lg:text-[150px] font-narrow font-bold text-center">
-          Quality f<span className="text-primary-orange">oo</span>ds
-        </h1>
-        <p className="text-center text-[18px] lg:text-[24px] uppercase tracking-[4px] lg:tracking-[9px] text-primary-orange font-narrow font-normal">
-          Healthy Food for healthy body
-        </p>
-      </div>
-
-      <Hero />
-    </section>
+    <div className="relative pb-[500px]">
+      {/* Rendering the Banner component at the top of the page */}
+      <Banner />
+      {/* Rendering the BannerInfo component below the Banner */}
+      <BannerInfo />
+    </div>
   );
 }
