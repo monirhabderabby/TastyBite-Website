f12304d68fbfaa29eec406da0c0edb374f8ba9fd
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-22 16:08:38 +0600

    feat: develop hero section with animated elements using Framer Motion
    - Implemented hero section layout with absolute positioning
    - Added animated image and leaves with staggered transitions
    - Leaf sizes responsive to screen size (150px for medium, 200px for large)
    - Used Framer Motion for smooth animations on image and leaf elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import FoodCart from "@/components/common/cards/food-card";
+import Hero from "@/components/ui/Hero";
 
 export default function Home() {
-    return (
-        <>
-            <FoodCart theme="light" />
-        </>
-    );
+  return (
+    <section
+      style={{ backgroundImage: `url(/images/banner-bg.webp)` }}
+      className=" min-h-[50vh] md:min-h-[70vh] lg:min-h-[90vh] bg-primary-black w-full relative overflow-hidden"
+    >
+      <div className="pt-[100px]">
+        <h1 className="uppercase text-[50px] lg:text-[150px] font-narrow font-bold text-center">
+          Quality f<span className="text-primary-orange">oo</span>ds
+        </h1>
+        <p className="text-center text-[18px] lg:text-[24px] uppercase tracking-[4px] lg:tracking-[9px] text-primary-orange font-narrow font-normal">
+          Healthy Food for healthy body
+        </p>
+      </div>
+
+      <Hero />
+    </section>
+  );
 }
