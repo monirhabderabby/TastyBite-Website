ed8641480ca50eda38c6287fda84481563d1f834
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-10-23 19:12:52 +0600

    Latest news section developed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import LatestNews from "@/components/home/latest-news";
 import Speciality from "@/components/home/speciality";
 import Banner from "@/components/ui/banner";
 import BannerInfo from "@/components/ui/BannerInfo";
 
 export default function Home() {
-  return (
-    <div className="relative">
-      {/* Rendering the Banner component at the top of the page */}
-      <Banner />
-      {/* Rendering the BannerInfo component below the Banner */}
-      <BannerInfo />
-      {/* Special category section */}
-      <Speciality />
-    </div>
-  );
+    return (
+        <div className="relative">
+            {/* Rendering the Banner component at the top of the page */}
+            <Banner />
+            {/* Rendering the BannerInfo component below the Banner */}
+            <BannerInfo />
+            {/* Special category section */}
+            <Speciality />
+
+            {/* Latest news section */}
+            <LatestNews />
+        </div>
+    );
 }
