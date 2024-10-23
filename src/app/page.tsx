0aa376dd6fa57a7210f4c424cd77174fe01510b3
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-23 15:42:01 +0600

    developed: fotter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Banner from "@/components/ui/banner";
 import BannerInfo from "@/components/ui/BannerInfo";
 
 export default function Home() {
-    return (
-        <div className="relative pb-[500px]">
-            {/* Rendering the Banner component at the top of the page */}
-            <Banner />
-            {/* Rendering the BannerInfo component below the Banner */}
-            <BannerInfo />
-            {/* Special category section */}
-            <Speciality />
-        </div>
-    );
+  return (
+    <div className="relative">
+      {/* Rendering the Banner component at the top of the page */}
+      <Banner />
+      {/* Rendering the BannerInfo component below the Banner */}
+      <BannerInfo />
+      {/* Special category section */}
+      <Speciality />
+    </div>
+  );
 }
