27ad976ce8eaf0f245f3b6dddf185fcff2edc9a4
Prosantaroy1 <royprosanta172@gmail.com>
2024-10-25 06:54:12 -0700

    add to aboutPage Banner section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,25 @@
+// import AboutTastybite from "@/components/about/AboutTastybite";
+// import OurStory from "@/components/about/OurStory";
+
+import AllPageBanner from "@/components/common/AllPageBanner/AllPageBanner";
+
+
 const Page = () => {
-  return <div>About</div>;
+  return <div className="">
+    {/* about banner section add */}
+    <div>
+      <AllPageBanner
+        img={'https://utfs.io/f/oI7Ou0bdQ6rjiGyuYM7CO0fsq7HAWnKhc6pERGMxZXFQizYo'}
+        title="ABOUT US"
+        activelink="About"
+      />
+    </div>
+    {/* Tastybite about section add */}
+    <div className="py-16">
+      {/* <AboutTastybite />
+      <OurStory /> */}
+    </div>
+  </div>;
 };
 
 export default Page;
