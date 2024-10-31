3e7fe0ddd715e73a0640504c78e1961938cc736e
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-31 21:49:48 +0600

    developed: filter container

--- src/app/foods/_components/foods-container.tsx
+++ src/app/foods/_components/foods-container.tsx
@@ -1,33 +1,13 @@
 import FoodCart from "@/components/common/cards/food-card/food-card";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import Sorting from "./filter/sorting";
 
 const FoodsContainer = () => {
   return (
     <div>
       <div className="lg:flex justify-end hidden">
-        <Select>
-          <SelectTrigger className="w-[180px] text-[#999999]">
-            <SelectValue placeholder="Sort By" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="light" className="text-[#999999]">
-              Best Selling
-            </SelectItem>
-            <SelectItem value="light" className="text-[#999999]">
-              Price (low to high)
-            </SelectItem>
-            <SelectItem value="light" className="text-[#999999]">
-              Price (high to low)
-            </SelectItem>
-          </SelectContent>
-        </Select>
+        <Sorting />
       </div>
+
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-y-[50px]">
         <FoodCart theme="light" />
         <FoodCart theme="light" />
--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -1,10 +1,14 @@
 import FoodsFilterContainer from "./_components/filter/foods-filter-container";
+import MobileFilterContainer from "./_components/filter/mobile-filter-container";
 import FoodsContainer from "./_components/foods-container";
 
 const Page = () => {
   return (
-    <div className="flex items-start gap-x-10 min-h-screen container mt-10">
-      <div className="w-[314px] flex-initial">
+    <div className="flex flex-col md:flex-row items-start gap-x-10 min-h-screen container space-y-10 mt-10">
+      <div className=" md:hidden w-full">
+        <MobileFilterContainer />
+      </div>
+      <div className="hidden md:block md:w-[314px] flex-initial">
         <FoodsFilterContainer />
       </div>
       <div className="flex-auto">
