482fe5939e1e0365041cf5fb5c4fea9c47407f4f
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-11-26 18:50:47 +0600

    fix: minified error

--- src/app/wizard/page.tsx
+++ src/app/wizard/page.tsx
@@ -1,11 +1,12 @@
-import WizardForm from "./_components/wizard-form";
+import dynamic from "next/dynamic";
+const WizardForm = dynamic(() => import("./_components/wizard-form"));
 
 const WizardPage = () => {
-    return (
-        <div className="container mx-auto mt-[70px] h-[calc(100vh-70px)] flex justify-center items-center">
-            <WizardForm />
-        </div>
-    );
+  return (
+    <div className="container mx-auto mt-[70px] h-[calc(100vh-70px)] flex justify-center items-center">
+      <WizardForm />
+    </div>
+  );
 };
 
 export default WizardPage;
