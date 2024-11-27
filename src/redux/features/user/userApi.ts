bdf3fb955ea807c0e8390d081fc499254c3c057c
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-11-27 21:23:24 +0600

    developed: profile edit

--- src/redux/features/user/userApi.ts
+++ src/redux/features/user/userApi.ts
@@ -1,59 +1,56 @@
 import { baseApi } from "../../api/baseApi";
 
 const userApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllUsers: builder.query({
-            query: () => ({
-                url: "/user",
-                method: "GET",
-            }),
-            providesTags: ["User"],
-        }),
-        getSingleUser: builder.query({
-            query: (id) => ({
-                url: `/user/${id}`,
-                method: "GET",
-            }),
-            providesTags: (result, error, id) => [{ type: "SingleUser", id }],
-        }),
-        createUser: builder.mutation({
-            query: (body) => ({
-                url: "/users",
-                method: "POST",
-                body,
-            }),
-            invalidatesTags: ["User"],
-        }),
-        updateUser: builder.mutation({
-            query: ({ body, id }) => ({
-                url: `/user/${id}`,
-                method: "PATCH",
-                body,
-            }),
-            invalidatesTags: (result, error, { id }) => [
-                "User",
-                { type: "SingleUser", id },
-            ],
-        }),
-        deleteUser: builder.mutation({
-            query: (id: string) => ({
-                url: `/user/${id}`,
-                method: "DELETE",
-            }),
-            invalidatesTags: (result, error, id) => [
-                "User",
-                { type: "SingleUser", id },
-            ],
-        }),
+  endpoints: (builder) => ({
+    getAllUsers: builder.query({
+      query: () => ({
+        url: "/user",
+        method: "GET",
+      }),
+      providesTags: ["User"],
     }),
+    getSingleUser: builder.query({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "GET",
+      }),
+      providesTags: (id) => [{ type: "SingleUser", id }],
+    }),
+    createUser: builder.mutation({
+      query: (body) => ({
+        url: "/users",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    updateUser: builder.mutation({
+      query: ({ body }) => ({
+        url: `/users/update-profile`,
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: () => ["User"],
+    }),
+    deleteUser: builder.mutation({
+      query: (id: string) => ({
+        url: `/user/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, id) => [
+        "User",
+        { type: "SingleUser", id },
+      ],
+    }),
+  }),
 });
 
 export const {
-    useGetAllUsersQuery,
-    useGetSingleUserQuery,
-    useCreateUserMutation,
-    useUpdateUserMutation,
-    useDeleteUserMutation,
+  useGetAllUsersQuery,
+  useGetSingleUserQuery,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
 } = userApi;
 
 export default userApi;
