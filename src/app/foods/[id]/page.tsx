4581f778948a3501224a6518cb93a962e440b8ec
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-11-29 23:20:25 +0600

    developed: ssg for dynamic food

--- src/app/foods/[id]/page.tsx
+++ src/app/foods/[id]/page.tsx
@@ -1,11 +1,21 @@
+import { TFood } from "@/types";
 import SingleFoodContainer from "./_components/single-food-container";
 
-const Page = ({ params }: { params: { id: string } }) => {
-    return (
-        <section className="container mt-[100px] space-y-[100px] pb-[150px]">
-            <SingleFoodContainer foodId={params.id} />
-        </section>
-    );
+export async function generateStaticParams() {
+  const posts = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/food`).then(
+    (res) => res.json()
+  );
+  const ids = posts?.data?.map((item: TFood) => ({ id: item._id }));
+
+  return ids;
+}
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  return (
+    <section className="container mt-[100px] space-y-[100px] pb-[150px]">
+      <SingleFoodContainer foodId={params.id} />
+    </section>
+  );
 };
 
 export default Page;
