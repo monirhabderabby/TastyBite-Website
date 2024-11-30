f1143ab3bef67d022423f3c8f75c0e6fcfae2531
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-11-30 21:00:01 +0600

    Footer link updated

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,69 +1,100 @@
+"use client";
+
 // Packages
 import Link from "next/link";
 
 // Local imports
 import Logo from "@/components/common/logo/Logo";
 import { footerLinks, socialLinks } from "@/data/footer";
+import { setMenu } from "@/redux/features/filter/filterSlice";
+import { useGetAllMenusQuery } from "@/redux/features/menu/menuApi";
+import { TMenu } from "@/types";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
 
 const Footer = () => {
-  return (
-    <footer className="bg-primary-black">
-      <section className="border-b border-white/10 py-[50px] md:py-[100px]">
-        <div className="container grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-x-8">
-          {/* 1st item */}
-          <div className="space-y-5">
-            <div className="space-y-2">
-              <Logo />
-              <p className="text-[14px] text-white">
-                20 Carrochan Rd, Balloch, Alexandria G83 8EG, UK 69QJ+2F
-                Alexandria, United Kingdom
-              </p>
-            </div>
-            <div className="space-y-2">
-              <p>PHONE: +8801956306002</p>
-              <p>EMAIL: info@tastybite.com</p>
-            </div>
-          </div>
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const { data: menuData } = useGetAllMenusQuery({});
 
-          {footerLinks.map(({ id, links, title }) => (
-            <div key={id}>
-              <h3 className="text-white text-[20px]">{title}</h3>
-              <div className="flex flex-col gap-y-2 mt-[20px]">
-                {links.map((item) => (
-                  <Link
-                    key={item.id}
-                    href={item.href}
-                    className="hover:text-primary-orange transition-colors duration-300 w-fit"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <CopyRightMarker />
-    </footer>
-  );
+    const handleMenuChange = (_id: string) => {
+        dispatch(setMenu(_id));
+        router.push(`/foods`);
+    };
+
+    return (
+        <footer className="bg-primary-black">
+            <section className="border-b border-white/10 py-[50px] md:py-[100px]">
+                <div className="container grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-x-8 xl:gap-x-16">
+                    {/* 1st item */}
+                    <div className="space-y-5">
+                        <div className="space-y-2">
+                            <Logo />
+                            <p className="text-[14px] text-white">
+                                20 Carrochan Rd, Balloch, Alexandria G83 8EG, UK
+                                69QJ+2F Alexandria, United Kingdom
+                            </p>
+                        </div>
+                        <div className="space-y-2">
+                            <p>PHONE: +8801956306002</p>
+                            <p>EMAIL: info@tastybite.com</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 className="text-white text-[20px]">TOP ITEMS</h3>
+                        <div className="flex flex-col gap-y-2 mt-[20px]">
+                            {menuData?.data.map((menu: TMenu) => (
+                                <p
+                                    key={menu.name}
+                                    className={`hover:text-primary-orange transition-colors duration-300 w-fit cursor-pointer`}
+                                    onClick={() => handleMenuChange(menu._id)}
+                                >
+                                    {menu.name}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+
+                    {footerLinks.map(({ id, links, title }) => (
+                        <div key={id}>
+                            <h3 className="text-white text-[20px]">{title}</h3>
+                            <div className="flex flex-col gap-y-2 mt-[20px]">
+                                {links.map((item) => (
+                                    <Link
+                                        key={item.id}
+                                        href={item.href}
+                                        className="hover:text-primary-orange transition-colors duration-300 w-fit"
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <CopyRightMarker />
+        </footer>
+    );
 };
 
 export default Footer;
 
 const CopyRightMarker = () => {
-  return (
-    <section className="h-[80px] w-full container flex justify-between items-center">
-      <h6 className="text-[14px]">
-        &copy; TastyBite all Rights Reserved. Designed by{" "}
-        <span className="text-primary-orange">NextGen Dev</span>
-      </h6>
-      <div className="flex items-center gap-x-3">
-        {socialLinks.map(({ href, icon: Icon, id, name }) => (
-          <a key={id} href={href} target={`_${name}`}>
-            <Icon className="h-5 w-5 text-white/80 transition-colors duration-300 hover:text-primary-orange" />
-          </a>
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section className="h-[80px] w-full container flex justify-between items-center">
+            <h6 className="text-[14px]">
+                &copy; TastyBite all Rights Reserved. Designed by{" "}
+                <span className="text-primary-orange">NextGen Dev</span>
+            </h6>
+            <div className="flex items-center gap-x-3">
+                {socialLinks.map(({ href, icon: Icon, id, name }) => (
+                    <a key={id} href={href} target={`_${name}`}>
+                        <Icon className="h-5 w-5 text-white/80 transition-colors duration-300 hover:text-primary-orange" />
+                    </a>
+                ))}
+            </div>
+        </section>
+    );
 };
--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,133 +1,96 @@
-import { Facebook, Instagram, LucideIcon, TwitterIcon } from "lucide-react";
+import { Facebook, Linkedin, LucideIcon } from "lucide-react";
 
 export type socialLinkProps = {
-  id: number;
-  name: string;
-  icon: LucideIcon;
-  href: string;
+    id: number;
+    name: string;
+    icon: LucideIcon;
+    href: string;
 };
 export type footerLinkProps = {
-  id: number;
-  name: string;
-  href: string;
+    id: number;
+    name: string;
+    href: string;
 };
 export type footerLinksProps = {
-  id: number;
-  title: string;
-  links: footerLinkProps[];
+    id: number;
+    title: string;
+    links: footerLinkProps[];
 };
 
 // social links
 export const socialLinks = [
-  {
-    id: 1,
-    name: "Facebook",
-    icon: Facebook,
-    href: "https://www.facebook.com",
-  },
-  {
-    id: 2,
-    name: "Instagram",
-    icon: Instagram,
-    href: "https://www.instagram.com",
-  },
-  {
-    id: 3,
-    name: "X",
-    icon: TwitterIcon,
-    href: "https://x.com/home",
-  },
+    {
+        id: 1,
+        name: "Facebook",
+        icon: Facebook,
+        href: "https://www.facebook.com",
+    },
+    {
+        id: 3,
+        name: "LinkedIn",
+        icon: Linkedin,
+        href: "https://www.linkedin.com/feed/",
+    },
 ] as socialLinkProps[];
 
 // footer links
 export const footerLinks = [
-  {
-    id: 1,
-    title: "INFORMATION",
-    links: [
-      {
+    {
         id: 1,
-        name: "Home",
-        href: "/",
-      },
-      {
+        title: "INFORMATION",
+        links: [
+            {
+                id: 1,
+                name: "Home",
+                href: "/",
+            },
+            {
+                id: 2,
+                name: "Blog",
+                href: "/blog",
+            },
+            {
+                id: 3,
+                name: "About Us",
+                href: "/about",
+            },
+            {
+                id: 4,
+                name: "Foods",
+                href: "/foods",
+            },
+            {
+                id: 5,
+                name: "Contact Us",
+                href: "/contact",
+            },
+        ],
+    },
+    {
         id: 2,
-        name: "Blog",
-        href: "/blog",
-      },
-      {
-        id: 3,
-        name: "About Us",
-        href: "/about",
-      },
-      {
-        id: 4,
-        name: "Foods",
-        href: "/foods",
-      },
-      {
-        id: 5,
-        name: "Contact Us",
-        href: "/contact",
-      },
-    ],
-  },
-  // {
-  //   id: 2,
-  //   title: "TOP ITEMS",
-  //   links: [
-  //     {
-  //       id: 1,
-  //       name: "Burrata Pizza",
-  //       href: "/",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "Cheese pizza",
-  //       href: "/",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Italian Pizza",
-  //       href: "/",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "Sollow Burger",
-  //       href: "/",
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "Double Cheese Pizza",
-  //       href: "/",
-  //     },
-  //   ],
-  // },
-  {
-    id: 3,
-    title: "USEFUL LINKS",
-    links: [
-      {
-        id: 1,
-        name: "Privacy Policy",
-        href: "/privacy-policy",
-      },
+        title: "USEFUL LINKS",
+        links: [
+            {
+                id: 1,
+                name: "Privacy Policy",
+                href: "/privacy-policy",
+            },
 
-      {
-        id: 3,
-        name: "Return Policy",
-        href: "/return-policy",
-      },
-      {
-        id: 4,
-        name: "Contact",
-        href: "/contact",
-      },
-      {
-        id: 5,
-        name: "My account",
-        href: "/profile",
-      },
-    ],
-  },
+            {
+                id: 3,
+                name: "Return Policy",
+                href: "/return-policy",
+            },
+            {
+                id: 4,
+                name: "Contact",
+                href: "/contact",
+            },
+            {
+                id: 5,
+                name: "My account",
+                href: "/profile",
+            },
+        ],
+    },
 ] as footerLinksProps[];
