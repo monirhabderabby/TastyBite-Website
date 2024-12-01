9a0a525f6db007a310221355c3d3c9e31b5388cb
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-12-01 22:36:38 +0600

    Navbar icon size updated

--- src/components/common/navbar/menu-end.tsx
+++ src/components/common/navbar/menu-end.tsx
@@ -4,9 +4,9 @@ import CartSheet from "@/components/cart/cart-sheet";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { selectCartTotalQuantity } from "@/redux/features/cart/cartSelector";
 import { RootState } from "@/redux/store";
@@ -18,138 +18,138 @@ import { useSelector } from "react-redux";
 import NavbarSearchModal from "./navbar-search-modal";
 
 const MenuEnd = ({
-  scrolling,
-  pathname,
-  unreadNotification,
+    scrolling,
+    pathname,
+    unreadNotification,
 }: {
-  scrolling: boolean;
-  pathname: string;
-  unreadNotification: number;
+    scrolling: boolean;
+    pathname: string;
+    unreadNotification: number;
 }) => {
-  const [open, setOpen] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
 
-  const { isSignedIn } = useUser();
-  const wishlist = useSelector((state: RootState) => state.wishlist.items);
-  const cartItemsNumber = useSelector(selectCartTotalQuantity);
+    const { isSignedIn } = useUser();
+    const wishlist = useSelector((state: RootState) => state.wishlist.items);
+    const cartItemsNumber = useSelector(selectCartTotalQuantity);
 
-  return (
-    <div className="hidden lg:flex items-center gap-x-4">
-      <Button
-        variant="ghost"
-        className="px-1 text-white hover:bg-transparent hover:text-primary-orange"
-        onClick={() => setOpen(!open)}
-      >
-        <Search className="w-6" />
-      </Button>
-      {isSignedIn && (
-        <Tooltip>
-          <TooltipTrigger>
-            <Link href="/notifications" className="relative">
-              <Bell className="w-5" />
-              <p
-                className={`absolute -top-2 -right-3 ${
-                  scrolling
-                    ? "bg-[#91b842] text-white"
-                    : "bg-white text-primary-black"
-                } w-5 h-5 flex items-center justify-center rounded-full`}
-              >
-                {unreadNotification}
-              </p>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent className="bg-primary-orange text-white">
-            Notifications
-          </TooltipContent>
-        </Tooltip>
-      )}
-      <Tooltip>
-        <TooltipTrigger>
-          <Link href="/wishlist" className="relative">
-            <Heart className="w-5" />
-            <p
-              className={`absolute -top-2 -right-3 ${
-                scrolling
-                  ? "bg-[#91b842] text-white"
-                  : "bg-white text-primary-black"
-              } w-5 h-5 flex items-center justify-center rounded-full`}
+    return (
+        <div className="hidden lg:flex items-center gap-x-4">
+            <Button
+                variant="ghost"
+                className="px-1 text-white hover:bg-transparent hover:text-primary-orange"
+                onClick={() => setOpen(!open)}
             >
-              {wishlist.length}
-            </p>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent className="bg-primary-orange text-white">
-          Wishlist
-        </TooltipContent>
-      </Tooltip>
-      {isSignedIn && (
-        <Tooltip>
-          <TooltipTrigger>
-            {pathname !== "/cart" && isSignedIn ? (
-              <Sheet>
-                <SheetTrigger asChild>
-                  <div className="relative">
-                    <ShoppingCart className="w-5" />
-                    <p
-                      className={`absolute -top-2 -right-3 ${
-                        scrolling
-                          ? "bg-[#91b842] text-white"
-                          : "bg-white text-primary-black"
-                      } w-5 h-5 flex items-center justify-center rounded-full`}
-                    >
-                      {cartItemsNumber}
-                    </p>
-                  </div>
-                </SheetTrigger>
-                <SheetContent className="border p-0">
-                  <CartSheet />
-                </SheetContent>
-              </Sheet>
-            ) : (
-              <Link href="/cart" className="relative">
-                <ShoppingCart className="w-5" />
-                <p
-                  className={`absolute -top-2 -right-3 ${
-                    scrolling
-                      ? "bg-[#91b842] text-white"
-                      : "bg-white text-primary-black"
-                  } w-5 h-5 flex items-center justify-center rounded-full`}
-                >
-                  {cartItemsNumber}
-                </p>
-              </Link>
+                <Search className="w-6" />
+            </Button>
+            {isSignedIn && (
+                <Tooltip>
+                    <TooltipTrigger>
+                        <Link href="/notifications" className="relative">
+                            <Bell className="w-5" />
+                            <p
+                                className={`absolute -top-2 -right-[10px] ${
+                                    scrolling
+                                        ? "bg-[#91b842] text-white"
+                                        : "bg-white text-primary-black"
+                                } w-[18px] h-[18px] text-[14px] flex items-center justify-center rounded-full`}
+                            >
+                                {unreadNotification}
+                            </p>
+                        </Link>
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-primary-orange text-white">
+                        Notifications
+                    </TooltipContent>
+                </Tooltip>
             )}
-          </TooltipTrigger>
-          <TooltipContent className="bg-primary-orange text-white">
-            Cart
-          </TooltipContent>
-        </Tooltip>
-      )}
-
-      {isSignedIn ? (
-        <UserButton />
-      ) : (
-        <SignedOut>
-          <SignInButton
-            fallbackRedirectUrl="/"
-            signUpFallbackRedirectUrl="/wizard"
-          >
             <Tooltip>
-              <TooltipTrigger>
-                <Link href="/sign-in ">
-                  <User className="w-5" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-primary-orange text-white">
-                Sign In
-              </TooltipContent>
+                <TooltipTrigger>
+                    <Link href="/wishlist" className="relative">
+                        <Heart className="w-5" />
+                        <p
+                            className={`absolute -top-2 -right-[10px] ${
+                                scrolling
+                                    ? "bg-[#91b842] text-white"
+                                    : "bg-white text-primary-black"
+                            } w-[18px] h-[18px] text-[14px] flex items-center justify-center rounded-full`}
+                        >
+                            {wishlist.length}
+                        </p>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent className="bg-primary-orange text-white">
+                    Wishlist
+                </TooltipContent>
             </Tooltip>
-          </SignInButton>
-        </SignedOut>
-      )}
+            {isSignedIn && (
+                <Tooltip>
+                    <TooltipTrigger>
+                        {pathname !== "/cart" && isSignedIn ? (
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <div className="relative">
+                                        <ShoppingCart className="w-5" />
+                                        <p
+                                            className={`absolute -top-2 -right-[10px] ${
+                                                scrolling
+                                                    ? "bg-[#91b842] text-white"
+                                                    : "bg-white text-primary-black"
+                                            } w-[18px] h-[18px] text-[14px] flex items-center justify-center rounded-full`}
+                                        >
+                                            {cartItemsNumber}
+                                        </p>
+                                    </div>
+                                </SheetTrigger>
+                                <SheetContent className="border p-0">
+                                    <CartSheet />
+                                </SheetContent>
+                            </Sheet>
+                        ) : (
+                            <Link href="/cart" className="relative">
+                                <ShoppingCart className="w-5" />
+                                <p
+                                    className={`absolute -top-2 -right-3 ${
+                                        scrolling
+                                            ? "bg-[#91b842] text-white"
+                                            : "bg-white text-primary-black"
+                                    } w-5 h-5 flex items-center justify-center rounded-full`}
+                                >
+                                    {cartItemsNumber}
+                                </p>
+                            </Link>
+                        )}
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-primary-orange text-white">
+                        Cart
+                    </TooltipContent>
+                </Tooltip>
+            )}
 
-      <NavbarSearchModal open={open} setOpen={setOpen} />
-    </div>
-  );
+            {isSignedIn ? (
+                <UserButton />
+            ) : (
+                <SignedOut>
+                    <SignInButton
+                        fallbackRedirectUrl="/"
+                        signUpFallbackRedirectUrl="/wizard"
+                    >
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="/sign-in ">
+                                    <User className="w-5" />
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent className="bg-primary-orange text-white">
+                                Sign In
+                            </TooltipContent>
+                        </Tooltip>
+                    </SignInButton>
+                </SignedOut>
+            )}
+
+            <NavbarSearchModal open={open} setOpen={setOpen} />
+        </div>
+    );
 };
 
 export default MenuEnd;
