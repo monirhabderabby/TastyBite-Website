1ba818d3c28337100e709a002ef453b9bf83eda2
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-12-11 16:36:32 +0600

    update: brand loader

--- src/components/ui/brand-loader.tsx
+++ src/components/ui/brand-loader.tsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
 interface Props {
   message: string;
 }
 
 const BrandLoader = ({ message }: Props) => {
+  const pathName = usePathname();
+  const home = pathName === "/";
+
   return (
-    <div className="min-h-screen w-full flex flex-col justify-center items-center opacity-80 gap-y-3">
-      <div className="font-courgette text-black font-semibold text-[40px]">
+    <div
+      className={cn(
+        "min-h-screen w-full flex flex-col justify-start pt-[180px] md:pt-[200px] lg:pt-[230px] items-center  gap-y-3 z-50",
+        home ? "bg-primary-black" : ""
+      )}
+      style={{ backgroundImage: `url(/images/banner-bg.webp)` }}
+    >
+      <div
+        className={cn(
+          "font-courgette  font-semibold text-[40px]",
+          home ? "text-white" : "text-primary-black"
+        )}
+      >
         <span className="text-primary-orange">Tasty</span>Bite
       </div>
       <div className="flex flex-col justify-center items-center">
         <div className="loader" />
-        <div className="mt-2 text-primary-black">{message}</div>
+        <div className={cn("mt-2", home ? "text-white" : "text-primary-black")}>
+          {message}
+        </div>
       </div>
     </div>
   );
