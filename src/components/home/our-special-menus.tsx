1f5a1d9d5c5a8e9196a137b35d54e9bec9d300b3
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-12-11 17:50:49 +0600

    fixed: food menu tab responsive issue

--- src/components/home/our-special-menus.tsx
+++ src/components/home/our-special-menus.tsx
@@ -1,28 +1,29 @@
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import SectionHeader from "../common/sectionHeader/sectionHeader";
 import BlackRubBG from "../ui/black-rub-bg";
-import MenusContaner from "./menus-container";
+const MenusContaner = dynamic(() => import("./menus-container"));
 
 const OurSpecialMenus = () => {
-    return (
-        <BlackRubBG placeholderImg={false}>
-            <SectionHeader
-                heading="Fresh from TastyBite"
-                title="Our Special Menu"
-                headingTextColor="text-white"
-            />
-            <MenusContaner />
+  return (
+    <BlackRubBG placeholderImg={false}>
+      <SectionHeader
+        heading="Fresh from TastyBite"
+        title="Our Special Menu"
+        headingTextColor="text-white"
+      />
+      <MenusContaner />
 
-            <div className="mt-16 flex justify-center">
-                <Link
-                    href={"/foods"}
-                    className="text-primary-orange hover:text-white underline duration-300 text-xl"
-                >
-                    Find All Foods
-                </Link>
-            </div>
-        </BlackRubBG>
-    );
+      <div className="mt-16 flex justify-center">
+        <Link
+          href={"/foods"}
+          className="text-primary-orange hover:text-white underline duration-300 text-xl"
+        >
+          Find All Foods
+        </Link>
+      </div>
+    </BlackRubBG>
+  );
 };
 
 export default OurSpecialMenus;
