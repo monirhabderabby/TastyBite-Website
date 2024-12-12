ba946e42d08d341777f701056bb8e2d65180c65f
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-12-12 21:19:31 +0600

    Fixed: Responsive Issue on delicious section on Tablet Devices

--- src/components/home/about-food.tsx
+++ src/components/home/about-food.tsx
@@ -11,7 +11,7 @@ const AboutFood = () => {
     return (
         <div>
             <div className="container overflow-hidden">
-                <div className="max-w-full md:max-w-[560px] md:mx-auto lg:max-w-full my-[50px] md:my-[100px] grid grid-cols-1 lg:grid-cols-2 gap-10 overflow-hidden">
+                <div className="max-w-full md:mx-auto my-[50px] md:my-[100px] grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-4 lg:gap-10 overflow-hidden">
                     <motion.div
                         initial={{ opacity: 0, x: -50 }}
                         whileInView={{ opacity: 1, x: 0 }}
@@ -35,20 +35,20 @@ const AboutFood = () => {
                             className="w-[520px]"
                         >
                             <p
-                                className={`text-primary-orange text-left text-xl md:text-2xl lg:text-[30px] font-pacifico mb-2 md:mb-3 lg:mb-5`}
+                                className={`text-primary-orange text-left text-xl md:text-xl lg:text-[30px] font-pacifico mb-2 md:mb-0 lg:mb-5`}
                             >
                                 About Our Food
                             </p>
                             <h2
                                 className={`text-primary-black
-                            text-left text-5xl md:text-6xl lg:text-[70px] uppercase font-bold tracking-wide mb-5`}
+                            text-left text-5xl md:text-3xl lg:text-[60px] lg:leading-[60px] xl:text-[70px] xl:leading-[70px] uppercase font-bold tracking-wide mb-5 md:mb-1 lg:mb-5`}
                             >
                                 Where Quality Meet Excellent{" "}
                                 <span className="text-primary-orange">
                                     Service.
                                 </span>
                             </h2>
-                            <p className="text-sm mb-6 text-primary-black/75 max-w-[480px] text-left">
+                            <p className="text-sm mb-6 md:mb-2 lg:mb-6 text-primary-black/75 max-w-[480px] text-left">
                                 It&apos;s the perfect dining experience where
                                 every dish is crafted with fresh, high-quality
                                 Experience quick and efficient service that
@@ -57,26 +57,26 @@ const AboutFood = () => {
                                 with fresh, high-quality ingredients
                             </p>
 
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 max-w-[480px] mb-6">
+                            <div className="grid grid-cols-2 md:grid-cols-2 gap-x-1 gap-y-5 max-w-[480px] mb-6 md:mb-3 lg:mb-6">
                                 <div className="flex items-center gap-3">
                                     <Award className="w-[55px] h-[55px] text-primary-orange" />
                                     <div>
-                                        <p className="text-primary-black text-base uppercase font-semibold">
+                                        <p className="text-primary-black text-sm xl:text-base uppercase font-semibold">
                                             Super quality food
                                         </p>
-                                        <p className="text-primary-black text-sm">
+                                        <p className="text-primary-black/75 text-xs xl:text-sm leading-5 md:leading-4 lg:leading-5">
                                             A team of dreamers and doers build
-                                            unique interactive music and art
+                                            unique interactive art
                                         </p>
                                     </div>
                                 </div>
                                 <div className="flex items-center gap-3">
                                     <Medal className="w-[55px] h-[55px] text-primary-orange" />
                                     <div>
-                                        <p className="text-primary-black text-base uppercase font-semibold">
+                                        <p className="text-primary-black text-sm xl:text-base uppercase font-semibold">
                                             Well reputation
                                         </p>
-                                        <p className="text-primary-black text-sm">
+                                        <p className="text-primary-black/75 text-xs xl:text-sm leading-5 md:leading-4 lg:leading-5">
                                             A team of reputed dreamers and doers
                                             build unique interactive art
                                         </p>
@@ -92,7 +92,7 @@ const AboutFood = () => {
                                     <p className="text-primary-orange uppercase text-sm font-semibold tracking-wide">
                                         Brendon Garry
                                     </p>
-                                    <p className="text-primary-black text-sm uppercase">
+                                    <p className="text-primary-black text-sm md:text-xs lg:text-sm xl:uppercase">
                                         Customer&apos;s experience is our
                                         highest priority.
                                     </p>
